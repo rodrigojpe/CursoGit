ae42a4ef2c5c64cde26a80e8bb4458639ed4b0d9
Rodrigo <academiacp@mail.com>
2017-05-23 15:49:56 -0400

    Agrgar dos botones al formulario

--- demo2/Form1.Designer.cs
+++ demo2/Form1.Designer.cs
@@ -39,6 +39,8 @@
             this.btnconectar = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -142,11 +144,33 @@
             this.button1.Text = "???";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(457, 451);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(781, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "???";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(457, 412);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(781, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "???";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1250, 498);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnconectar);
@@ -173,6 +197,8 @@
         private System.Windows.Forms.Button btnconectar;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
